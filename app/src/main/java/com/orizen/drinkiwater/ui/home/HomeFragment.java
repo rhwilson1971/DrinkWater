16b637f3bb4db6775b12d0481839213a80dfff91
Reuben Wilson <reuben.h.wilson@gmail.com>
2023-07-01 21:16:47 -0700

    Pie chart additions

--- app/src/main/java/com/orizen/drinkiwater/ui/home/HomeFragment.java
+++ app/src/main/java/com/orizen/drinkiwater/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.orizen.drinkiwater.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +13,13 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.orizen.drinkiwater.databinding.FragmentHomeBinding;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private PieChart pieChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +29,10 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        pieChart = binding.piechart;
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
         return root;
     }
 
@@ -33,5 +40,28 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void setData() {
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        "R",
+//                        Integer.parseInt(tvR.getText().toString()),
+//                        Color.parseColor("#FFA726")));
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        "Python",
+//                        Integer.parseInt(tvPython.getText().toString()),
+//                        Color.parseColor("#66BB6A")));
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        "C++",
+//                        Integer.parseInt(tvCPP.getText().toString()),
+//                        Color.parseColor("#EF5350")));
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        "Java",
+//                        Integer.parseInt(tvJava.getText().toString()),
+//                        Color.parseColor("#29B6F6")));
     }
 }
